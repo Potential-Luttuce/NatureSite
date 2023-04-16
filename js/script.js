8ccd3d2ae47171b489e6d9d9b3f09140bac6b60b
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-16 17:44:46 -0400

    Commit Miker Cuesta!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // Show about & nature section popup
 function about() {
-   alert("Sorry - The About section is under construction");
+   // alert("Sorry - The About section is under construction");
+   const div = document.createElement('div');
+   div.style.cssText = 'colr'
 }
 function nature() {
     alert("Sorry - The Nature section is under construction");
