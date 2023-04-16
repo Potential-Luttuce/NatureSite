b7a136457178da0cd05e129b977520d56f76ef62
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-16 13:53:37 -0400

    Created js folder for scripts and functions - addeed functionality to about and nature buttons

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Show about & nature section popup
+function about() {
+   alert("Sorry - The About section is under construction");
+}
+function nature() {
+    alert("Sorry - The Nature section is under construction");
+ }
