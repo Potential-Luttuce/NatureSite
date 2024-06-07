10c95b33ba17a297b4637918030da33a43536014
Mike <Cuestamichael0@gmail.com>
2024-06-07 16:09:05 -0400

    Committing, small adjustments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-// Show about & nature section popup
-function about() {
-   alert("Sorry - The About section is under construction");
-   // const about = document.querySelector('#aboutContainer');
-   // about.classList.toggle('hide');  
+function construction() {
+   Swal.fire ({
+      title: 'Currently under construction, Sorry.',
+      icon: 'info',
+      color: 'grey'
+   })
 }
-function alert() {
-    alert("Sorry - This section is under construction");
- }
-
- function dark() {
-   // const page = document.querySelectorAll('#DarkMode');
-   // page.style.cssText = 'background-color: white;';
-   alert('Currently this feature is under Construction.');
- }
+   function alert() {
+   construction()
+}
